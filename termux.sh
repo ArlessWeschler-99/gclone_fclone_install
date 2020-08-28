5f107fea17f3df82b248dd1b78d981940b2e9858
arlessweschler <62666455+arlessweschler@users.noreply.github.com>
2020-08-28 16:53:22 -0500

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+CLDBIN=/data/data/com.termux/files/usr/bin/gclone
+OSARCH=$(uname -m)
+BINTAG=Linux_arm64
+wget -qO- https://api.github.com/repos/donwa/gclone/releases/latest \
+| grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
+| wget --no-verbose -i- -O- | gzip -d -c > ${CLDBIN}
+chmod 0755 ${CLDBIN}
+
+gclone version
