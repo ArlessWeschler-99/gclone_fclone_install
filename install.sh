919a95941019209e7a3b2bf90c2c4ac861d27b86
arlessweschler <62666455+arlessweschler@users.noreply.github.com>
2020-08-28 03:47:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,32 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
 CLDBIN=/usr/bin/gclone
 OSARCH=$(uname -m)
-BINTAG=Linux_arm64
+case $OSARCH in 
+    x86_64)
+        BINTAG=Linux_x86_64
+        ;;
+    i*86)
+        BINTAG=Linux_i386
+        ;;
+    arm64)
+        BINTAG=Linux_arm64
+        ;;
+    arm*)
+        BINTAG=Linux_armv6
+        ;;
+    *)
+        echo "unsupported OSARCH: $OSARCH"
+        exit 1
+        ;;
+esac
+
 wget -qO- https://api.github.com/repos/donwa/gclone/releases/latest \
 | grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
 | wget --no-verbose -i- -O- | gzip -d -c > ${CLDBIN}
