6ecf65d163f598e23436bff44cb4f3d6df64b1db
arlessweschler <62666455+arlessweschler@users.noreply.github.com>
2020-08-29 00:03:48 -0500

    Update fclonetmx.sh

--- fclonetmx.sh
+++ fclonetmx.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
+CLDBIN=/data/data/com.termux/files/usr/bin/
+OSARCH=$(uname -m)
+BINTAG=linux-arm64
 wget -qO- https://api.github.com/repos/mawaya/rclone/releases/latest \
-  | grep browser_download_url | grep linux-arm64 | cut -d '"' -f 4 \
-  | wget --no-verbose -i- && unzip -j fclone*.zip -d /usr/bin && chmod 0755 /usr/bin/fclone
+| grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
+| wget --no-verbose -i- && unzip -j fclone*.zip -d ${CLDBIN} && chmod 0755 /data/data/com.termux/files/usr/bin/fclone
+
+fclone version && cd $HOME && rm -r fclone-v0.3.1-linux-arm64.zip
+
+echo "Fclone installed successfully. Fclone instalado satisfactoriamente"
