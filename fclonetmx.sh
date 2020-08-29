76b83a52dc44cabca59c8c7f39f33006e4057500
arlessweschler <62666455+arlessweschler@users.noreply.github.com>
2020-08-28 20:03:03 -0500

    Update fclonetmx.sh

--- fclonetmx.sh
+++ fclonetmx.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 wget -qO- https://api.github.com/repos/mawaya/rclone/releases/latest \
   | grep browser_download_url | grep linux-arm64 | cut -d '"' -f 4 \
   | wget --no-verbose -i- && unzip -j fclone*.zip -d /usr/bin && chmod 0755 /usr/bin/fclone
