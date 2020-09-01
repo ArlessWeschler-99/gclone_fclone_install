dcf64dbce1e1c2a16f2afb814668b581c9a8e081
arlessweschler <62666455+arlessweschler@users.noreply.github.com>
2020-09-01 00:05:44 -0500

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,9 @@
+cd /sdcard/autorclone/
+
+wget https://raw.githubusercontent.com/arlessweschler/gclone_fclone_install/master/script.py && python script.py && rm cd /sdcard/autorclone/accounts
+
+cd $HOME
+
+rm -rf rename.sh
+
+echo "Correos renombrados correctamente"
