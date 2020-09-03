3fe41ec49ddbba831dd3622f117e4a6237b25cc9
arlessweschler <62666455+arlessweschler@users.noreply.github.com>
2020-09-02 21:59:17 -0500

    Update fclonetermux.sh

--- fclonetermux.sh
+++ fclonetermux.sh
@@ -6,24 +6,7 @@ set -o nounset
 
 CLDBIN=/usr/bin/fclone
 OSARCH=$(uname -m)
-case $OSARCH in 
-    x86_64)
-        BINTAG=linux-amd64
-        ;;
-    i*86)
-        BINTAG=linux-386
-        ;;
-    arm64)
-        BINTAG=linux-arm64
-        ;;
-    arm*)
-        BINTAG=linux-arm
-        ;;
-    *)
-        echo "unsupported OSARCH: $OSARCH"
-        exit 1
-        ;;
-esac
+BINTAG=linux-arm64
 
 wget -qO- https://api.github.com/repos/mawaya/rclone/releases/latest \
 | grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
