2d52cb9ecdf8f342d3dff18b22b214fdb4c289d8
arlessweschler <62666455+arlessweschler@users.noreply.github.com>
2020-09-04 19:08:13 -0500

    Update fclonetermux.sh

--- fclonetermux.sh
+++ fclonetermux.sh
@@ -28,7 +28,7 @@ esac
 wget -qO- https://api.github.com/repos/mawaya/rclone/releases/latest \
 | grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
 | wget --no-verbose -i-
-unzip -j fclone*.zip -d /data/data/com.termux/files/usr/bin
+unzip -j fclone*.zip -d /data/data/com.termux/files/usr/bin/
 rm fclone*.zip
 chmod 0755 ${CLDBIN}
 
